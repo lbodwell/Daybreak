ebd634b20cd19030c99f391029d11b2a202ab82c
Nathan Klingensmith <njklingensmith@wpi.edu>
2022-03-25 16:03:08 -0400

    Switch Drake from Pawn to Character, add basic Idle/Run/Attack animations

---
+++ Source/Daybreak/DaybreakEnemyCharacter.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "DaybreakEnemyCharacter.generated.h"
+
+UCLASS()
+class DAYBREAK_API ADaybreakEnemyCharacter : public ACharacter {
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ADaybreakEnemyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Animations)
+    UAnimMontage* AttackMontage;
+	
+	/** Whether enemy is currently attacking and should not be able to attack again yet. */
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category=State)
+	bool Attacking;
+
+	void Attack();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
